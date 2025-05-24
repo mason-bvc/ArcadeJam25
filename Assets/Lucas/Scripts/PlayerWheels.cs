c3a76d71df1cf0a0e163d3d7e92ab53414f143f4
LucasLadan <l.ladan419@mybvc.ca>
2025-05-24 14:34:25 -0600

    Updated scene

--- Assets/Lucas/Scripts/PlayerWheels.cs
+++ Assets/Lucas/Scripts/PlayerWheels.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class PlayerWheels : MonoBehaviour
 {
+
+    [SerializeField] private PlayerControl _player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         
     }
+
+    
 }
