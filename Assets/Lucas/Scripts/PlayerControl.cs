cd3c8bf66d5064fd07e8e13ca1dbd24be44f9e61
LucasLadan <l.ladan419@mybvc.ca>
2025-05-24 16:39:22 -0600

    SOUNDS

--- Assets/Lucas/Scripts/PlayerControl.cs
+++ Assets/Lucas/Scripts/PlayerControl.cs
@@ -8,8 +8,11 @@ public class PlayerControl : MonoBehaviour
 
     [SerializeField] private InputActionReference _playerMovement;
     [SerializeField] private float _speed;
+    [SerializeField] private float _breakSpeed;
     [SerializeField] private float _maxSpeed;
     [SerializeField] private float _laneSwitchSpeed;
+    [SerializeField] private AudioSource _motorSound;
+    [SerializeField] private AudioSource _explosionSound;
     [SerializeField] private ParticleSystem _explosion;
     [SerializeField] private CinemachineCamera _cam;
     [SerializeField] private Collider _triggerer;
@@ -38,16 +41,23 @@ public class PlayerControl : MonoBehaviour
     {
         if (_isAlive && other.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("Collision happened");
-            _isAlive = false;
-            onDeath.Invoke();
-            _explosion.Emit(500);
-            _rigidbody.maxLinearVelocity = 10000;
-            _rigidbody.freezeRotation = false;
-            _rigidbody.AddForce(0,Mathf.Abs(GetCurrentVelocity())*2,0);
-            _cam.gameObject.SetActive(false);
-            _triggerer.enabled = false;
+            Explode();
         }
+    }
+
+    public void Explode()
+    {
+        Debug.Log("Collision happened");
+        _isAlive = false;
+        onDeath.Invoke();
+        _explosionSound.Play();
+        _motorSound.Stop();
+        _explosion.Emit(500);
+        _rigidbody.maxLinearVelocity = 10000;
+        _rigidbody.freezeRotation = false;
+        _rigidbody.AddForce(0, Mathf.Abs(GetCurrentVelocity()) * 2, 0);
+        _cam.gameObject.SetActive(false);
+        _triggerer.enabled = false;
     }
 
     private void Movement()//X is Left/Right, Y is Up/Down
@@ -58,7 +68,18 @@ public class PlayerControl : MonoBehaviour
 
         vector2Movement.y -= 0.33f;
 
-        Vector3 movement = new Vector3(vector2Movement.x * _laneSwitchSpeed, 0,vector2Movement.y * _speed);
+        Vector3 movement = new Vector3(0, 0, 0);
+
+        if (vector2Movement.y < 0)
+        {
+            movement = new Vector3(vector2Movement.x * _laneSwitchSpeed, 0, vector2Movement.y * _breakSpeed);
+        }
+        else
+        {
+            movement = new Vector3(vector2Movement.x * _laneSwitchSpeed, 0, vector2Movement.y * _speed);
+        }
+
+        
 
         if (GetCurrentVelocity() + movement.z < 0)
         {
@@ -67,7 +88,9 @@ public class PlayerControl : MonoBehaviour
         else
         {
             _rigidbody.AddForce(movement);
+            _motorSound.pitch = (GetCurrentVelocity()+10)/25;
         }
+        _motorSound.volume = GetCurrentVelocity()/10;
         
     }
 
@@ -78,6 +101,16 @@ public class PlayerControl : MonoBehaviour
     {
         _maxSpeed = newSpeed;
         _rigidbody.maxLinearVelocity = _maxSpeed;
+    }
+
+    public void ChangeSpeed(float newSpeed)
+    {
+        _speed = newSpeed;
+    }
+
+    public void ChangeBrakeSpeed(float newSpeed)
+    {
+        _breakSpeed = newSpeed;
     }
 
 }
