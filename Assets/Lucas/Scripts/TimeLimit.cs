683bb2874c8cd4932a7223255ef3a58e0bdfaca5
BowVallyCA <c.anderson965@mybvc.ca>
2025-05-25 12:46:42 -0600

    Adding UI Elements To Project
    Finally got t he Ui to work in the project and have made the speedometer and timer

--- Assets/Lucas/Scripts/TimeLimit.cs
+++ Assets/Lucas/Scripts/TimeLimit.cs
@@ -1,20 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class TimeLimit : MonoBehaviour
 {
-    [SerializeField] private PlayerControl _player;
     [SerializeField] private float _timeLimit;
-
+    [SerializeField] private TMP_Text _timeText;
+ 
     private void Update()
     {
         
         if (_timeLimit > 0)
         {
             _timeLimit -= Time.deltaTime;
+            _timeText.SetText(_timeLimit.ToString());
         }
         else
         {
-            _player.Explode();
+            FindAnyObjectByType<PlayerControl>().Explode();
             _timeLimit = 0;
         }
     }
