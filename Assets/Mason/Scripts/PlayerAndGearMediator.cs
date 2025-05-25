0f3a06639828841e985ed66b9a469ddab8e47602
LucasLadan <l.ladan419@mybvc.ca>
2025-05-25 10:16:28 -0600

    Updated stats for car

--- Assets/Mason/Scripts/PlayerAndGearMediator.cs
+++ Assets/Mason/Scripts/PlayerAndGearMediator.cs
@@ -14,21 +14,26 @@ public class PlayerAndGearMediator : MonoBehaviour
         {
             switch (gear)
             {
-            case GearShiftHandler.Gear.Neutral:
-                _playerControl.ChangeSpeed(0);
-                break;
-            case GearShiftHandler.Gear.First:
-                _playerControl.ChangeSpeed(45);
-                break;
-            case GearShiftHandler.Gear.Second:
-                _playerControl.ChangeSpeed(60);
-                break;
-            case GearShiftHandler.Gear.Third:
-                _playerControl.ChangeSpeed(75);
-                break;
-            case GearShiftHandler.Gear.Fourth:
-                _playerControl.ChangeSpeed(90);
-                break;
+                case GearShiftHandler.Gear.Neutral:
+                    _playerControl.ChangeTurnSpeed(0);
+                    _playerControl.ChangeMaxSpeed(0);
+                    break;
+                case GearShiftHandler.Gear.First:
+                    _playerControl.ChangeTurnSpeed(30);
+                    _playerControl.ChangeMaxSpeed(45);
+                    break;
+                case GearShiftHandler.Gear.Second:
+                    _playerControl.ChangeTurnSpeed(30);
+                    _playerControl.ChangeMaxSpeed(60);
+                    break;
+                case GearShiftHandler.Gear.Third:
+                    _playerControl.ChangeTurnSpeed(30);
+                    _playerControl.ChangeMaxSpeed(75);
+                    break;
+                case GearShiftHandler.Gear.Fourth:
+                    _playerControl.ChangeTurnSpeed(30);
+                    _playerControl.ChangeMaxSpeed(90);
+                    break;
             }
         });
     }
